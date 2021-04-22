97b57b5c63467e16da53176f7d2185ab9b9036ba
Robert Hensing <robert@roberthensing.nl>
2021-04-22 13:06:31 +0200

    devshell: hercules-ci -> hercules-ci (amended)

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { system ? builtins.currentSystem }:
 let
   src = 
-    fetchTarball "https://github.com/hercules-ci/devshell/archive/345e2d56212ad4b87dc53d67fcb917232b8d1611.tar.gz";
+    fetchTarball "https://github.com/hercules-ci/devshell/archive/b77379d11978d2d21fe73745f327481370d61f75.tar.gz";
   nixpkgs =
     fetchTarball "https://github.com/NixOS/nixpkgs/archive/0a5f5bab0e08e968ef25cff393312aa51a3512cf.tar.gz";
   pkgs =
