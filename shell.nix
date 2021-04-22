12dfdda046e8bd1e217cec6ce01decaab8f457dd
Robert Hensing <robert@roberthensing.nl>
2021-04-22 11:53:46 +0200

    Fix the devshell pin

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
 { system ? builtins.currentSystem }:
 let
   src = 
-    #fetchTarball "https://github.com/numtide/devshell/archive/master.tar.gz";
-    ../devshell;
+    fetchTarball "https://github.com/hercules-ci/devshell/archive/ef69e36493c201d7eebfcf84e51956f57319a2eb.tar.gz";
   devshell = import src { inherit system; };
 in
 devshell.fromTOML ./devshell.toml
