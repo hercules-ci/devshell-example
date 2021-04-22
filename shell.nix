e9d8107b69b6795a822f1272e81c17123ec5f5d2
Robert Hensing <robert@roberthensing.nl>
2021-04-22 12:10:44 +0200

    devshell: hercules-ci -> hercules-ci (amended)

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { system ? builtins.currentSystem }:
 let
   src = 
-    fetchTarball "https://github.com/hercules-ci/devshell/archive/ef69e36493c201d7eebfcf84e51956f57319a2eb.tar.gz";
+    fetchTarball "https://github.com/hercules-ci/devshell/archive/6ad3532e6596ea52b071cd44745f81308d076d24.tar.gz";
   devshell = import src { inherit system; };
 in
 devshell.fromTOML ./devshell.toml
